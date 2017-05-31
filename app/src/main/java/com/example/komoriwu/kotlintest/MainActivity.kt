f9df8a095b2ae3b8d8e36a7398ff0bc83e8b0185
KomoriWu <564139666@qq.com>
2017-05-31 10:45:57 -0700

    5-31 anko

--- app/src/main/java/com/example/komoriwu/kotlintest/MainActivity.kt
+++ app/src/main/java/com/example/komoriwu/kotlintest/MainActivity.kt
@@ -3,20 +3,32 @@ package com.example.komoriwu.kotlintest
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
-import android.widget.TextView
+import android.widget.EditText
+import org.jetbrains.anko.find
+import org.jetbrains.anko.toast
+import java.lang.Integer.parseInt
 
 class MainActivity : AppCompatActivity() {
-    var etText = "123"
-    var tv: TextView? = null
+    var etText: String? = null
+    var et: EditText? = null
     var btn: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tv = findViewById(R.id.textView) as TextView?
-        btn = findViewById(R.id.button) as Button?
-        etText = tv?.text as String
-        btn?.setOnClickListener() {
-            toast(etText)
+        et = find(R.id.et)
+        btn = find(R.id.button)
+        btn?.setOnClickListener() { toast(str(parseInt(et!!.text.toString()))) }
+    }
+
+    var sum = { x: Int, y: Int -> x + y }
+
+    var str = { x: Int ->
+        when (x) {
+            0 -> "输入0"
+            in 1..3 -> "输入1-3"
+            else -> {
+                "输入 $x"
+            }
         }
     }
 }
